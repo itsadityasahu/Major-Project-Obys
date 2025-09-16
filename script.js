7bfa8110e95644537e2ad64ef57d11d4093583a3
Aditya Kumar Gupta <askadityasahu@gmail.com>
2025-09-16 16:55:54 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
+function loadingAnimation(){
+    
 let tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
-    // duration:0.6,
-    // delay:0.5,
+    duration:0.6,
+    delay:0.5,
 });
 tl.from("#line1-part1",{
     opacity:0,
@@ -34,10 +35,36 @@ tl.from("#page1",{
     delay:0.2,
     y:1600,
     opacity:0,
-    duration:0.5,
+    duration:0.6,
     ease:Power4
 })
 
 tl.to("#loader",{
     display:"none"
 })
+
+tl.from("#nav",{
+    opacity:0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2, #hero3 h3, #hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+
+loadingAnimation()
+
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+});
+Shery.makeMagnet("#nav-part2 h4");
+}
+
+cursorAnimation()
